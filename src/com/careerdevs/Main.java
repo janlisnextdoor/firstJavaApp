51a11b934cabb1f86915495eb3bb1fd96eb8c2be
janlisnextdoor <janlisnextdoor@gmail.com>
2021-12-20 18:13:43 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,12 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(getFirstElement(new int[] {3, 1, 4}));
+    }
+    public static int getFirstElement (int[] arr) {
+        return arr[0];
+    }
+}
